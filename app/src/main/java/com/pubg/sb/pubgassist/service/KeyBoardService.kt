bce2eb9233aa3024d1069214467daf73b5c65155
xy <735450893@qq.com>
2019-01-23 16:58:40 +0800

    前方有敌人

--- app/src/main/java/com/pubg/sb/pubgassist/service/KeyBoardService.kt
+++ app/src/main/java/com/pubg/sb/pubgassist/service/KeyBoardService.kt
@@ -30,19 +30,62 @@ class KeyBoardService : AccessibilityService() {
 
     private fun performKeyPress(event: KeyEvent?) {
         if (event?.keyCode == KeyEvent.KEYCODE_F1) {
-            Thread {
-                try {
-                    Thread.sleep(50)
-                    typeIn(KeyEvent.KEYCODE_6)
-                    Thread.sleep(50)
-                    typeIn(KeyEvent.KEYCODE_5)
-                    Thread.sleep(50)
-                    typeIn(KeyEvent.KEYCODE_4)
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
-                }
-            }.start()
+            performF1()
+        }else if (event?.keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
+            performDPAD_LEFT()
+        }else if (event?.keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) {
+            performDPAD_RIGHT()
         }
+    }
+
+    /**
+      * 切换高倍镜(按键 方向右键)
+      */
+    private fun performDPAD_RIGHT() {
+        Thread {
+            try {
+                Thread.sleep(50)
+                typeIn(KeyEvent.KEYCODE_6)
+                Thread.sleep(50)
+                typeIn(KeyEvent.KEYCODE_4)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+
+    /**
+      * 切换低倍镜(按键 方向左键)
+      */
+    private fun performDPAD_LEFT() {
+        Thread {
+            try {
+                Thread.sleep(50)
+                typeIn(KeyEvent.KEYCODE_6)
+                Thread.sleep(50)
+                typeIn(KeyEvent.KEYCODE_5)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+
+    /**
+      * 前方有敌人(按键F1)
+     */
+    private fun performF1() {
+        Thread {
+            try {
+                Thread.sleep(50)
+                typeIn(KeyEvent.KEYCODE_J)
+                Thread.sleep(50)
+                typeIn(KeyEvent.KEYCODE_K)
+                Thread.sleep(50)
+                typeIn(KeyEvent.KEYCODE_L)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }.start()
     }
 
 
